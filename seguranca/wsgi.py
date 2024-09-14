d378657264d1614b0247e53eed911bb55ce59805
Maxsuênaldy Alves <maxpnn@hotmail.com>
2024-09-14 15:36:50 -0300

    atualizacao w

--- seguranca/wsgi.py
+++ seguranca/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for seguranca project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seguranca.settings')
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
